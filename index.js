b6d10b82bce42fd192df657f476581ec132c0d44
Jonathan Yeung <jonathanyeung1997@gmail.com>
2018-07-29 01:00:01 +1000

    kappa123

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-function gumtreeScrape(search){	
-	const rp = require('request-promise');
-	const cheerio = require('cheerio');
-	let all = [];
+let memes = [];
+let all = [];
 
+
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+function gumtreeScrape(search){	
 	const options = {
-		url: 'https://www.gumtree.com.au/s-'+search+'/k0?sort=price_desc',
+		uri: 'https://www.gumtree.com.au/s-'+search+'/k0?sort=price_desc',
 		transform: function (body) {
 	    return cheerio.load(body);
 	  }
 	};
-	rp(options)
+	
+	return rp(options)
 	  .then(function ($) {
 		let first = $('.user-ad-collection--row .user-ad-collection__list-wrapper');
 		let child = first.children();
 		child.each(function(i, elem) {
 			let namePrice = $(this).attr('aria-label');
 			let url = $(this).attr('href');
-			all.push([namePrice, url]);
-		});
-		return all;//		console.log(all);
-	  });
-//	console.log(items);
-//	  .catch(function (err) {
-//	    console.log(err);
-//	  });
-//	return items;
-}
+			if(namePrice != undefined && url != undefined){
+				all.push([namePrice, url])};
+		return JSON.parse($.all);
+	  })
+	
+	  .catch(function (err) {
+	    console.log(err);
+	  }).finally(function() {
+		
+		return all;
+	});
+});
 
-console.log(gumtreeScrape('2k17'));
+memes = (gumtreeScrape('2k17'));
+console.log(memes);
