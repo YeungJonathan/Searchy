7cef455adc96a632497ff829fd88b696a41bcd6f
Jonathan Yeung <jonathanyeung1997@gmail.com>
2018-07-28 21:37:20 +1000

    Couldnt return the list

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
+function gumtreeScrape(search){	
+	const rp = require('request-promise');
+	const cheerio = require('cheerio');
+	let all = [];
 
+	const options = {
+		url: 'https://www.gumtree.com.au/s-'+search+'/k0?sort=price_desc',
+		transform: function (body) {
+	    return cheerio.load(body);
+	  }
+	};
+	rp(options)
+	  .then(function ($) {
+		var items = [];
+		let first = $('.user-ad-collection--row .user-ad-collection__list-wrapper');
+		let child = first.children();
+		child.each(function(i, elem) {
+			let namePrice = $(this).attr('aria-label');
+			let url = $(this).attr('href');
+			items.push([namePrice, url]);
+			all.push([namePrice, url]);
+		});
+		return all;
+//		console.log(items);
+	  });
+//	console.log(items);
+//	  .catch(function (err) {
+//	    console.log(err);
+//	  });
+//	return items;
+}
 
-const options = {
-	uri: `https://www.gumtree.com.au/s-2k17/k0?sort=price_desc`,
-	transform: function (body) {
-    return cheerio.load(body);
-  }
-};
-
-rp(options)
-  .then(function ($) {
-
-	let first = $('.user-ad-collection--row .user-ad-collection__list-wrapper');
-	let child = first.children();
-	items = [];
-	child.each(function(i, elem) {
-		let namePrice = $(this).attr('aria-label');
-		let url = $(this).attr('href');
-		items.push([namePrice, url]);
-	});	
-	console.log(items);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+console.log(gumtreeScrape('2k17'));
