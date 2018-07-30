43f2d97a5b5f680684a5c6834a652fa0e1d5b6bd
Jonathan Yeung <jonathanyeung1997@gmail.com>
2018-07-30 15:03:00 +1000

    ..

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
-# UniHack-2018
+# Searchy
+🛍️🎁📺🛏️📦💰📗💎
 
-Team 20 - HAL (Hopeless, Aimless, Lifeless)
+We made this project in 24 hours in Unihack 2018.
 
-We're making a website that would help people compare prices between several sources - Amazon, e-Bay, and Gumtree, for now.
+The concept is to create a website that compares different online shopping platforms (Amazon, Ebay and Gumtree for now) when user searches for a particular item in the search bar of our website. There will be 3 columns showing top results from each website. Images, names, descriptions, prices and the link will be shown to users for each items. 
+
+There is also a chart showing the average prices for each website for that particular products for good measures for the users. There are different filters for 
+the user to choose from.
+
+Filter includes:
+- Cheapest
+- Most Expensive
+- Most Relevant
+- Most Recent
+
+We get the data from the 3 websites by scraping datas and using APIs. Then we will filter the most relevant data and present it to the user on the website.
+
+Users can compare prices and details for each results side by side to minimize their time needed to compare manually.
+
+In future, we plan on comparing other websites as well so that more products could be compared and making the website more user-friendly.
+
+Project made in nodeJS and  HTML.
+
+## Development Team
+- [Jonathan Yeung](https://github.com/YeungJonathan)
+- [Marcus Ooi](https://github.com/MarcusKJOoi)
+- [Zain Shroff](https://github.com/zain610)
+
+## Trying it out
+Requirement:
+- Node.js
+- Cheerio
+- Request Promise
+
+Steps:
+
+1. Clone and Download the project files.
+2. Open a Terminal and run the app.js file.
+3. Open http://localhost:8080/
--- app.js
+++ app.js
@@ -4,9 +4,15 @@ var request = require('request');
 var http = require('http');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const ama = require('ama');
+const ama = require('./ama');
 
 let gumtreeData = [];
+let amazonItems = [];
+amazonItems = amazonSearch(1);
+let array = {
+	gumtree: gumtreeData,
+	amazon: amazonItems
+}
 
 
 var hostname = '127.0.0.1';
@@ -67,22 +73,9 @@ app.get('/results', function(req,res){
     	if(response.statusCode == 200 && !error){
     			// var data = JSON.parse(body)
     			gumtreeSearch(searchitem, 1, function(result) {
-<<<<<<< HEAD
+
     				if(result != undefined ) {
-//    					console.log(result);
-						for (let i = 0; i<5 ; i++){
-							let tmp = []
-							a = result[i]
-							b = a[0].split('\n')
-							tmp.push(b[0]);
-							tmp.push(b[1].slice(6,b[1].length));
-							tmp.push('https://www.gumtree.com.au'+a[1]);
-							items.push(tmp);
-						}
-    					res.render('results', {items:items});
-    				}
-    			});
-=======
+
     					for (let i = 0; i<5 ; i++){
     						let tmp = []
     						a = result[i]
@@ -92,27 +85,26 @@ app.get('/results', function(req,res){
     						tmp.push('https://www.gumtree.com.au'+a[1]);
     						gumtreeData.push(tmp);
     					}
-       					console.log(gumtreeData);
-    					res.render('results', {gumtreeData:gumtreeData});
 
-    				});
-    			}
->>>>>>> 952a02dde08dfed501cee8640cc19cff4c1163bd
-    			
-    		
+    					// console.log(gumtreeData);
+    					console.log(array)
+    					res.render('results', {array:array});
+    				}
+    				else{
+    					console.log("error occured, code:" + error)
+    					console.log("statusCode" + response.statusCode)
+    				}
 
-    		else{
-    			console.log("error occured, code:" + error)
-    			console.log("statusCode" + response.statusCode)
+    			});
     		}
-    	})
+    	});
+});
 
-})
 
 
-app.get('/graph', function(req, res){
-	res.render('graph')
-})
-app.listen(port, hostname, function(){
-	console.log("Server has started")
-})
+    		app.get('/graph', function(req, res){
+    			res.render('graph')
+    		})
+    		app.listen(port, hostname, function(){
+    			console.log("Server has started")
+    		})
